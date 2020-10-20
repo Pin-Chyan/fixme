4e682546dc76867bbcc5c5ed659087696aa57815
Pin-Chyan <50664297+Pin-Chyan@users.noreply.github.com>
2020-10-20 18:15:51 +0200

    Update Broker.java

--- broker/src/main/java/com/endlesshorizon/broker/Broker.java
+++ broker/src/main/java/com/endlesshorizon/broker/Broker.java
@@ -3,6 +3,8 @@ package com.endlesshorizon.broker;
 import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.endlesshorizon.broker.utils.Prefixes;
 import com.endlesshorizon.broker.valids.Messages;
@@ -11,6 +13,7 @@ public class Broker {
 	private static final String SERVER_IP = "localhost";
 	private static final int SERVER_PORT = 5000;
 	static Boolean connected = false;
+	static String UID;
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 		Socket client = new Socket(SERVER_IP, SERVER_PORT);
@@ -24,9 +27,9 @@ public class Broker {
 			String serverResponse = input.readLine();
 			System.out.println(serverResponse);
 			if (client.isConnected()) {
-				//getBrokerID(serverResponse);
+				getBrokerID(serverResponse);
 				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-				System.out.print(Prefixes.FM_BCON + "_>");
+				System.out.print(Prefixes.FM_BCON + "UID:" + UID +"_>");
 				String command = keyboard.readLine();
 	
 				if (Messages.validFormat(command)) {
@@ -44,7 +47,11 @@ public class Broker {
 		System.exit(0);
 	}
 	
-	//private static void getBrokerID(String response) {
-		
-	//}
+	private static void getBrokerID(String response) {
+		final Pattern p = Pattern.compile( "(\\d{6})" );
+		final Matcher m = p.matcher(response);
+		if ( m.find() ) {
+		  UID = m.group( 0 );
+		}
+	}
 }
