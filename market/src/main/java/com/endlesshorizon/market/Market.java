90297b3ac364b139bf7254c48388f568e7c43a2c
hbarnardWTC <wingarkorin@gmail.com>
2020-11-06 15:22:50 +0200

    Goodbye readline

--- market/src/main/java/com/endlesshorizon/market/Market.java
+++ market/src/main/java/com/endlesshorizon/market/Market.java
@@ -20,26 +20,13 @@ public class Market {
 
     public static void main(String[] args) throws UnknownHostException, IOException {
 		Socket client = new Socket(SERVER_IP, SERVER_PORT);
-
-		BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-
-		// keyboard inputs which are getting read in
-		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
 		
 		while (true) {
 			String serverResponse = input.readLine();
 			System.out.println(serverResponse);
 			if (client.isConnected()) {
 				getMarketID(serverResponse);
-				PrintWriter out = new PrintWriter(client.getOutputStream(), true);
 				System.out.print("> ");
-				String command = keyboard.readLine();
-				
-				out.println(command);
-	
-				if (command.equals("exit")) {
-					break;
-				}
 			}
 		}
 
